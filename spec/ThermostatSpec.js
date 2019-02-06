f3e7bea217aa8ee8308971f0dc2c198e076e70e5
thomasmarano <thomas.marano2@gmail.com>
2019-02-06 18:29:23 +0000

    Added PowerSaving and various other features

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -11,42 +11,65 @@ describe("Thermostat", function(){
 
     it('lets you increase the temperature with an up function', function(){
       var x = thermostat.temperature();
-      thermostat.up();
+      thermostat.up(3);
       expect(thermostat.temperature()).toBeGreaterThan(x);
     });
 
     it('lets you decrease the temperature with a down function', function(){
       var y = thermostat.temperature();
-      thermostat.down();
+      thermostat.down(3);
       expect(thermostat.temperature()).toBeLessThan(y);
     });
 
-    it('raises an error if user treis to put temperature below 10 degrees', function(){
+    it('only lets the user put temperature down', function(){
+      //turn power saving mode off
+      // expect(function(){thermostat.down(11);}).toThrowError('Exceeds Minimum Temperature');
+      thermostat.down(3);
+      expect(thermostat.temperature()).toEqual(17)
+    })
+
+    it('only lets the user put temperature to the min temperature', function(){
+      //turn power saving mode off
+      // expect(function(){thermostat.down(11);}).toThrowError('Exceeds Minimum Temperature');
+      thermostat.down(20);
+      expect(thermostat.temperature()).toEqual(10)
+    })
+
+    it("won't raise temperature above 32 degrees", function(){
+    thermostat.adjustPS(false);
+    thermostat.up(20);
+    expect(thermostat.temperature()).toEqual(32)
+    })
+
+    it("confirms that thermostat starts with power saving mode on", function(){
+    expect(thermostat.powerSaving()).toEqual(true)
+    });
+
+    it("lets you turn off power saving mode", function(){
+    thermostat.adjustPS(false)
+    expect(thermostat.powerSaving()).toEqual(false)
+    })
+
+    it("only allows max temperature of 25 degrees if power saving mode is on", function(){
 
 
     })
 });
 
 
-//
-// it("The 'toBeGreaterThan' matcher is for mathematical comparisons", function() {
-//     var pi = 3.1415926,
-//       e = 2.78;
-//
-//     expect(pi).toBeGreaterThan(e);
-//     expect(e).not.toBeGreaterThan(pi);
-//   });
 
 
+
+
+// You can reset the temperature to 20 with a reset function
+// You can ask about the thermostat's current energy usage: < 18 is low-usage, < 25 is medium-usage, anything else is high-usage.
+// (In the challenges where we add an interface, low-usage will be indicated with green, medium-usage indicated with black, high-usage indicated with red.)
+
+
+// If power saving mode is on, the maximum temperature is 25 degrees
+// Power saving mode is on by default
+// If power saving mode is off, the maximum temperature is 32 degrees
 // Thermostat starts at 20 degrees
 //  You can increase the temperature with an up function
 // You can decrease the temperature with a down function
 // The minimum temperature is 10 degrees
-
-
-// If power saving mode is on, the maximum temperature is 25 degrees
-// If power saving mode is off, the maximum temperature is 32 degrees
-// Power saving mode is on by default
-// You can reset the temperature to 20 with a reset function
-// You can ask about the thermostat's current energy usage: < 18 is low-usage, < 25 is medium-usage, anything else is high-usage.
-// (In the challenges where we add an interface, low-usage will be indicated with green, medium-usage indicated with black, high-usage indicated with red.)
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,5 +1,10 @@
 function Thermostat() {
       this.temp = 20;
+      this.minTemp = 10;
+      this.maxTemp = 32;
+      this.psmaxTemp = 25;
+      this.psmode = true
+
 };
 
 
@@ -7,10 +12,38 @@ Thermostat.prototype.temperature = function() {
     return this.temp;
 };
 
-Thermostat.prototype.up = function() {
-    this.temp += 1;
+Thermostat.prototype.minTemperature = function() {
+    return this.minTemp;
 };
 
-Thermostat.prototype.down = function() {
-    this.temp -= 1;
+Thermostat.prototype.up = function(amount) {
+    if (this.powerSaving()) {
+        if ((this.temperature() + amount) >= 25) {
+            this.temp = 25;
+        } else {
+          this.temp += amount;
+        }
+    }   else if ((this.temperature() + amount) >= 32) {
+          this.temp = 32
+    } else {
+      this.temp += amount;
+    }
+};
+
+Thermostat.prototype.powerSaving = function() {
+    return this.psmode;
+}
+
+Thermostat.prototype.adjustPS = function(value) {
+    this.psmode = value
+}
+
+
+
+Thermostat.prototype.down = function(amount) {
+    if ((this.temperature() - amount) <= 10) {
+      this.temp = 10
+    } else {
+      this.temp -= amount
+    }
 };
